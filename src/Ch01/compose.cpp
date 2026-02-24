08fa99a205589eab00dbddacab070c53f3f687a4
Graham Strickland <grahamhstrickland@gmail.com>
2026-02-24 21:18:46 +0200

    Add C++ answers for exercises 1.4.1 and 1.4.2

---
+++ src/Ch01/compose.cpp
@@ -0,0 +1,27 @@
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <numbers>
+
+template <typename F, typename G, typename T> T compose(F f, G g, T x) {
+  return f(g(x));
+}
+
+int main(int argc, char *argv[]) {
+  using namespace std::numbers;
+
+  assert(compose([](double x) { return std::asin(x); },
+                 [](double x) { return std::sin(x); }, pi / 2.0) == pi / 2.0);
+  assert(compose([](double x) { return std::sin(x); },
+                 [](double x) { return std::asin(x); }, pi / 4.0) == pi / 4.0);
+  assert(compose([](double x) { return std::acos(x); },
+                 [](double x) { return std::cos(x); }, pi / 4.0) == pi / 4.0);
+  assert(compose([](double x) { return std::cos(x); },
+                 [](double x) { return std::acos(x); }, pi / 4.0) == pi / 4.0);
+  assert(compose([](double x) { return std::atan(x); },
+                 [](double x) { return std::tan(x); }, pi / 4.0) == pi / 4.0);
+  assert(compose([](double x) { return std::tan(x); },
+                 [](double x) { return std::atan(x); }, 0.0) == 0.0);
+
+  return EXIT_SUCCESS;
+}
